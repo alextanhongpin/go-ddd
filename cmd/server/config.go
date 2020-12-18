b08eccd9a0dd488d27050acf4a31eb23b0193faa
alextanhongpin <alextan220990@gmail.com>
2020-12-18 20:43:25 +0800

    chore(cmd/server): separate controller

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
-	Port int    `envconfig:"PORT" default:"8080"`
-	Env  string `envconfig:"ENV" default:"development"`
+	Port       int       `envconfig:"PORT" default:"8080"`
+	Env        string    `envconfig:"ENV" default:"development"`
+	GitCommit  string    `envconfig:"GIT_COMMIT" required:"true"`
+	DeployedAt time.Time `envconfig:"-"`
+	Host       string    `envconfig:"-"`
 }
 
 func NewConfig() Config {
-	cfg := Config{}
+	host, err := os.Hostname()
+	if err != nil {
+		log.Fatalf("hostnameError: %v", err)
+	}
+
+	cfg := Config{
+		DeployedAt: time.Now(),
+		Host:       host,
+	}
+
 	if err := envconfig.Process("", &cfg); err != nil {
 		log.Fatalf("parseConfigError: %v", err)
 	}
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,29 +2,15 @@
 
 package main
 
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
 func main() {
-	// Init usecase.
-	//uc := usercrud.New(nil)
-	//ctl := NewUserController(uc.Service)
-	//_ = ctl
-	//u, err := uc.Service.FindOne(context.Background(), "1")
-	//if err != nil {
-	//log.Fatal(err)
-	//}
 	cfg := NewConfig()
 	r, stop := newRouter(cfg)
 	defer stop()
 
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"ok": true,
-		})
-	})
+	{
+		ctl := NewHealthController(cfg)
+		r.GET("/health", ctl.GetHealth)
+	}
+
 	newServer(cfg, r)
 }
