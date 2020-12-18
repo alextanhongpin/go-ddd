2db2cb85266b5845ebfa1e2b322ef99583c5910d
alextanhongpin <alextan220990@gmail.com>
2020-12-18 17:37:18 +0800

    chore(cmd/server): add config

--- cmd/server/config.go
+++ cmd/server/config.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Port int    `envconfig:"PORT" default:"8080"`
+	Env  string `envconfig:"ENV" default:"development"`
+}
+
+func NewConfig() Config {
+	cfg := Config{}
+	if err := envconfig.Process("", &cfg); err != nil {
+		log.Fatalf("parseConfigError: %v", err)
+	}
+	return cfg
+}
